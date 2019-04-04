1befa9e66cc8c42713bfd845775b93a053b85a56
Robson Alves <robson.alves@fastalelo.onmicrosoft.com>
2019-04-04 19:42:48 -0300

    exercicio 1 - python e net

---
+++ ex1/net/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace net
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 10;
+            var b = 20;
+            var c = a;
+            a = b;
+            b = c;
+
+            Console.WriteLine($"Inverted - A={a} and B={b}");
+        }
+    }
+}
